d80f0e44d48293d4a9f3ce8f05f71cc9623d3d46
AhmedShaiban <143512500+AhmedShaiban@users.noreply.github.com>
2023-11-09 18:10:49 +0100

    While_Loop

---
+++ main.js
@@ -0,0 +1,93 @@
+//Opdracht 1
+let     i = 0
+while (i <= 9){ 
+   console.log(i);
+i++;
+}
+console.log("Einde programma"); 
+
+
+//Opdracht 2
+i = 2
+while (i <= 20)
+   { console.log(i);
+    i += 1
+
+}
+console.log("Einde programma");
+
+
+//Opdracht 3
+i = 10;
+while (i >= 0)
+{ console.log(i);
+    i -= 1
+
+}
+  
+console.log("Einde programma");
+
+//Opdracht 4
+i = 40;
+while (i >= 4){
+     console.log(i);
+    i -= 1
+
+}
+  
+console.log("Einde programma");
+
+//Opdracht 5
+i = 0;
+while (i < 8)
+{ console.log("Ik ben een student van ROC Mondriaan");
+    i += 1
+
+}
+  
+console.log("Einde programma");
+
+
+//Opdracht 6
+i = 0;
+while (i <= 12) {
+  console.log(`Dit is loop: ${i}`);
+  i++;
+}
+
+//Opdracht 7
+ i = 0;
+while (true) {
+  console.log(i);
+  i++;
+  if (i === 10) {
+    break;
+  }
+}
+console.log("Einde programma");
+
+//Opdracht 8
+i = 0;
+while (i < 20) {
+  console.log("Loop");
+  i++;
+  if (i === 10) {
+    break;
+  }
+}
+console.log("Einde programma");
+
+
+//Opdracht 9
+const eersteZin = "Dit is de eerste zin";
+const tweedeZin = "Dit is de tweede zin";
+ i = 0;
+while (i < 20) {
+  if (i < 10) {
+    console.log(eersteZin, tweedeZin);
+  } else {
+    console.log(eersteZin);
+  }
+  i++;
+}
+console.log("Einde programma");
